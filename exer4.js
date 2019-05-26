90bc799ff6443b153385d1e11511258c3f4a8f54
Jaguso <jagutzo@gmail.com>
2019-05-25 21:27:42 -0500

    solution for exer4

---
+++ exer4.js
@@ -0,0 +1,11 @@
+//Factorial function
+
+function factorialize(num) {
+    var result = 1;
+    for (let i=1; i<=num; i++) {
+      result *=i
+    }
+    return result;
+  }
+  
+  factorialize(5);
