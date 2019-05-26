4a6e0f1946ac5918a3177185c2b9ab857146fef1
Jaguso <jagutzo@gmail.com>
2019-05-26 16:41:51 -0500

    exer11

---
+++ exer11.js
@@ -0,0 +1,15 @@
+// We'll pass you an array of two numbers. Return the sum of those 
+// two numbers plus the sum of all the numbers between them.
+// The lowest number will not always come first.
+
+function sumAll(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    var sum = 0;
+    for (let i=min; i<=max; i++){
+        sum += i
+    }
+    return sum;
+}
+
+console.log(sumAll([1, 4]));
