8e1f2c709cae1b83c374fcd464e71fc5715fd147
Jaguso <jagutzo@gmail.com>
2019-05-31 23:53:22 -0500

    added notes

---
+++ freecc/exer21.js
@@ -0,0 +1,18 @@
+
+function whatIsInAName(collection, source) {
+
+    for (let i=0; i<collection.length; i++) {
+
+    }
+
+    collection.filter(elem[0])
+    var arr = [];
+    return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+const arr = [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }]
+const {x: first} = arr[0]
+console.log(x);
---
+++ notes/DataStructures.js
@@ -0,0 +1,7 @@
+
+//About push() and unshift()
+Both methods take one or more elements as parameters 
+and add those elements to the array the method is being 
+called on; the push() method adds elements to the end 
+of an array, and unshift() adds elements to the beginning.
+
