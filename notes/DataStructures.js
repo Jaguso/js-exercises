6f584ba36df1dd9f38db312fcabded97b3bde994
Jaguso <jagutzo@gmail.com>
2019-06-01 00:01:41 -0500

    notes

--- notes/DataStructures.js
+++ notes/DataStructures.js
@@ -5,3 +5,14 @@ and add those elements to the array the method is being
 called on; the push() method adds elements to the end 
 of an array, and unshift() adds elements to the beginning.
 
+//ejemplo
+function mixedNumbers(arr) {
+    arr.unshift('I', 2, 'three')
+    arr.push(7, 'VIII', 9)
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+----------------------
+
