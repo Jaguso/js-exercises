a4b941da85e669f258ad91fd4660b5ea5d34c9db
Jaguso <jagutzo@gmail.com>
2019-05-31 22:10:40 -0500

    notes

--- notes/ES6.js
+++ notes/ES6.js
@@ -61,3 +61,21 @@ const squareList = (arr) => {
 // test your code
 const squaredIntegers = squareList(realNumberArray);
 console.log(squaredIntegers);
+
+------------------------------------------------
+// Para una función con un numero variable de argumentos, tenemos
+
+function howMany(...args) {
+    return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2)); // You have passed 3 arguments
+console.log(howMany("string", null, [1, 2, 3], { })); // You have passed 4 arguments.
+
+--------------------------------------------
+// Podemos usar la notación 'spread' para calcular max y min de
+// un array.
+
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr); // returns 89
+
+
