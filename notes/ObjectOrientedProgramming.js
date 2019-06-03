15104390052a0f4c567b9e2aca68451f6e47f231
Jaguso <jagutzo@gmail.com>
2019-06-03 17:35:39 -0500

    b

---
+++ notes/ObjectOrientedProgramming.js
@@ -0,0 +1,11 @@
+// Esto es un ejemplo de un constructor 
+
+function Bird() {
+    this.name = "Albert";
+    this.color = "blue";
+    this.numLegs = 2;
+    // "this" inside the constructor always refers to the object being created
+}
+
+let blueBird = new Bird();
+
