8e19cd2edf92c96c08d6afb5aaea93cd395fbaf9
Jaguso <jagutzo@gmail.com>
2019-06-06 22:17:30 -0500

    check exer21

--- freecc/exer21.js
+++ freecc/exer21.js
@@ -1,13 +1,39 @@
+// Make a function that looks through an array of objects 
+// (first argument) and returns an array of all objects 
+// that have matching name and value pairs (second argument).
+// Each name and value pair of the source object has to be 
+// present in the object from the collection if it is to be 
+// included in the returned array.
+
+//EXAMPLE
+
+whatIsInAName([
+    { first: "Romeo", last: "Montague" }, 
+    { first: "Mercutio", last: null }, 
+    { first: "Tybalt", last: "Capulet" }
+    ], 
+    { last: "Capulet" }) should return 
+    [{ first: "Tybalt", last: "Capulet" }].   
+
+
 
 function whatIsInAName(collection, source) {
-
+    let final = [];
     for (let i=0; i<collection.length; i++) {
+        for (let key in collection[i]) {
+            if (collection[i][key] === source[key]) {
+                final.push(collection[i])
+            }
+        }
 
-    }
+        // for (collection[i][param] in obj) {
 
-    collection.filter(elem[0])
-    var arr = [];
-    return arr;
+        // }
+    }  
+    return final;
+    // collection.filter(elem[0])
+    // var arr = [];
+    // return arr;
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
--- notes/DataStructures.js
+++ notes/DataStructures.js
@@ -188,3 +188,49 @@ Alan
 Jeff
 Sarah
 Ryan
+
+-----------------------------
+// Un ejemplo mas complicado
+// IMPORTANTE
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+};
+  
+//OBS: obj.user son strings no objetos
+function countOnline(obj) {
+    let count = 0;
+    for (let user in obj) {
+        if (obj[user].online === true) { //OBS: tenemos que usar esta notacion
+            count = count + 1
+        }
+    }
+    return count;
+}
+
+console.log(countOnline(users));
+
+---------------------------------------
+Object.keys(obj)
+// devuelve los keys del objeto obj 
+// ejem:
+
+function getArrayOfUsers(obj) {
+    return Object.keys(obj);
+}
+
+console.log(getArrayOfUsers(users));
