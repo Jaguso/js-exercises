47295423b827d96c2ee7009b7bebe0b21e0548d4
Jaguso <jagutzo@gmail.com>
2019-06-06 19:16:47 -0500

    added notes

--- notes/DataStructures.js
+++ notes/DataStructures.js
@@ -29,10 +29,35 @@ console.log(popShift(['challenge', 'is', 'not', 'complete']));
 //logs: challenge complete
 
 -----------------------------------------
-.slice() xamples
+.slice() examples
 
 let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
 
 let todaysWeather = weatherConditions.slice(1, 3);
 // todaysWeather equals ['snow', 'sleet'];
 // weatherConditions still equals ['rain', 'snow', 'sleet', 'hail', 'clear']
+
+-----------------------------------------
+// spread operator: Cuando ponemos (...arr), ie un array que no tiene 
+// un numero determinado de elementos. 
+// OBS: lo usamos para copiar un array
+
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray];
+// thatArray equals [true, true, undefined, false, null]
+// thisArray remains unchanged, and is identical to thatArray
+
+-----------------------------------------
+// Podemos 'pegar' arrays dentro de otros arrays: 
+
+// Another huge advantage of the spread operator, is the ability 
+// to combine arrays, or to insert all the elements of one array 
+// into another, at any index. With more traditional syntaxes, 
+// we can concatenate arrays, but this only allows us to combine 
+// arrays at the end of one, and at the start of another. Spread 
+// syntax makes the following operation extremely simple:
+
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+// thatArray now equals ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
