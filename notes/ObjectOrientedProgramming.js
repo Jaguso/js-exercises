bc12f90c9a7a074299a8f09dc61db6af759835b6
Jaguso <jagutzo@gmail.com>
2019-06-07 19:31:06 -0500

    added notes

--- notes/ObjectOrientedProgramming.js
+++ notes/ObjectOrientedProgramming.js
@@ -9,3 +9,21 @@ function Bird() {
 
 let blueBird = new Bird();
 
+-----------------------------------
+// Creamos un constructor en el que podemos variar sus propiedades
+// al llamarlo
+
+function Bird(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+}
+
+let cardinal = new Bird("Bruce", "red");
+
+cardinal.name // => Bruce
+cardinal.color // => red
+cardinal.numLegs // => 2
+
+------------------------------------
+
