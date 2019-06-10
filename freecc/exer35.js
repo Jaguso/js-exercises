e554a492b386da0214ac41748160bc452853031d
Jaguso <jagutzo@gmail.com>
2019-06-09 23:50:52 -0500

    exer35

--- freecc/exer35.js
+++ freecc/exer35.js
@@ -14,15 +14,57 @@
 // sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
 // sym([1, 2, 3], [5, 2, 1, 4, 5]) should return [3, 4, 5].
 
-function sym(...args) {
-    let final = [];
-    for (let i=0; i<args.length; i++) {
-        args.splice(i,1,0)
-        
-    }
 
-    return args;
+// Esta funcion tiene de entrada un array y de salida el array sin 
+// valores repetidos
+function uniqueElements(arr) {
+    let final = [];
+    for (let i=0; i<arr.length; i++) {
+        if (!final.includes(arr[i])) {
+            final.push(arr[i])
+        }
+    }
+    return final;
 }
 
-console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+// console.log(uniqueElements([1,1,1,1,2]))
+
+// Esta función cuenta las veces que aparece elem en el array arr
+function countElem(arr, elem) {
+    let count = 0;
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] === elem) {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+// console.log(countElem([1,1,1,3,4,2], 2))
+
+function sym(...args) {
+    let middle = [];
+    for (let i=0; i<args.length; i++) {
+        for (let j=0; j<uniqueElements(args[i]).length; j++) {
+            middle.push(uniqueElements(args[i])[j])
+        }
+    }
+    let final = [];
+    for (let i=0; i<middle.length; i++) {
+        if (countElem(middle, middle[i]) === 1) {
+            final.push(middle[i]);
+        }
+    }
+
+    return final;
+}
+
+console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
+
+
+console.log('asdio'.includes('o'))
+
+var arr = [1,4,34,2,0,9]
+arr.sort((a,b) => a-b)
+console.log(arr);
 
