80a3cdfafb7a35e36bfabda92ab6da8615cf827c
Jaguso <jagutzo@gmail.com>
2019-06-09 22:06:34 -0500

    exer34 works

--- freecc/exer34.js
+++ freecc/exer34.js
@@ -1,22 +1,56 @@
+// Fill in the object constructor with the following methods below:
+
+// getFirstName() getLastName() getFullName() setFirstName(first) 
+// setLastName(last) setFullName(firstAndLast)
+
 var Person = function(firstAndLast) {
-    this.getFirstName = function() {
-        return firstAndLast.split(' ')[0];
-    }
-    this.getLastName() = function() {
-        return firstAndLast.split(' ')[1];
-    }
+    
+    this.firstAndLast = firstAndLast;
+    
     this.getFullName = function() {
-        return firstAndLast
+        return this.firstAndLast
     };
-    this.setFirstName(first) = function(first) {
-        firstAndLast.split(' ').splice(0,1, first);
+
+
+    this.getFirstName = function() {
+        return this.firstAndLast.split(' ')[0];
+    };
+    this.getLastName = function() {
+        return this.firstAndLast.split(' ')[1];
+    };
+    this.setFirstName = function(first) {
+        let arr = this.firstAndLast.split(' ');
+        arr.splice(0,1,first);
+        this.firstAndLast = arr.join(' ');
+    };
+    this.setLastName = function(last) {
+        let arr = this.firstAndLast.split(' ');
+        arr.splice(1,1,last);
+        this.firstAndLast = arr.join(' ');
+
+    };
+    this.setFullName = function(firstAndLast) {
+        this.firstAndLast = firstAndLast;
     }
-    this.setLastName(last) = function(last) {
-        firstAndLast.split(' ').splice(1,1,last);
-    }
-    // this.setFullName(firstAndLast) = function(firstAndLast)
-    //     firstAndLast.split().splice(0,1, firstAndLast);
+    
 };
 
 var bob = new Person('Bob Ross');
-bob.getFullName();
+console.log(bob.getFullName()); // Bob Ross
+console.log(bob.getFirstName()); // Bob
+console.log(bob.getLastName()); // Ross
+
+bob.setFirstName('Luis');
+bob.setLastName('Galvan');
+console.log(bob.getFullName()); // Luis Galvan
+bob.setFullName('Rafa Marquez'); 
+console.log(bob.getFullName()); // Rafa Marquez
+
+
+
+// Esto funciona para setFirstName
+var str = 'Luis Martinez';
+let newStr = str.split(' ');
+newStr.splice(0,1, 'Martin');
+// newStr.join(' ');
+console.log(newStr.join(' '));
